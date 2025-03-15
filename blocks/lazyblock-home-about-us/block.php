832b76bce6bec95ecba7f801a34502c2c2f6ead4
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2025-03-15 04:17:44 +0000

    feat: add about us section

--- blocks/lazyblock-home-about-us/block.php
+++ blocks/lazyblock-home-about-us/block.php
@@ -17,25 +17,9 @@
       </div>
       <div class="home-about__text-wrapper">
         <h2 class="home-about__heading"><?php echo esc_html($attributes['home-about-heading']) ?></h2>
-        <h3 class="home-about__subheading"><?php echo esc_html($attributes['home-about-subheading']) ?></h3>
-        <?php foreach ($attributes['home-about-text'] as $paragraph): ?>
-          <p class="home-about__text">
-            <?php echo esc_html($paragraph['paragraph']); ?>
-          </p>
-        <?php endforeach; ?>
+        <p class="home-about__subheading"><?php echo esc_html($attributes['home-about-text']) ?></p>
 
-        <div class="home-about__features-container">
-          <div class="home-about__feature home-about__feature--left">
-            <h4 class="home-about__feature-heading "><span class="counter" data-target="5">0</span>+</h4>
-            <p class="home-about__feature-subheading">Years of experience</p>
-            <p class="home-about__feature-text">We have over five years of proffesional experience</p>
-          </div>
-          <div class="home-about__feature home-about__feature--right">
-            <h4 class="home-about__feature-heading "><span class="counter" data-target="200">0</span>+</h4>
-            <p class="home-about__feature-subheading">Happy customers</p>
-            <p class="home-about__feature-text">We have served more than 100 happy customers</p>
-          </div>
-        </div>
+
 
       </div>
 
