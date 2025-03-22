f851ea94d3e67f9fb193496000376e31d092b827
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2025-03-22 07:11:20 +0000

    feat: customize login + my account pages

--- woocommerce/myaccount/dashboard.php
+++ woocommerce/myaccount/dashboard.php
@@ -1,81 +1,44 @@
 <?php
-/**
- * My Account Dashboard
- *
- * Shows the first intro screen on the account dashboard.
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/myaccount/dashboard.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see     https://woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 4.4.0
- */
-
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
-$allowed_html = array(
-	'a' => array(
-		'href' => array(),
-	),
-);
+$current_user = wp_get_current_user();
 ?>
 
-<p>
-	<?php
-	printf(
-		/* translators: 1: user display name 2: logout url */
-		wp_kses( __( 'Hello %1$s (not %1$s? <a href="%2$s">Log out</a>)', 'woocommerce' ), $allowed_html ),
-		'<strong>' . esc_html( $current_user->display_name ) . '</strong>',
-		esc_url( wc_logout_url() )
-	);
-	?>
-</p>
+<div class="custom-account-dashboard">
+	<div class="custom-account-header">
+		<h2><?php printf(esc_html__('Welcome, %s!', 'woocommerce'), esc_html($current_user->display_name)); ?></h2>
+		<p>
+			<?php
+			printf(
+				wp_kses(__('Not you? <a href="%1$s">Log out</a>', 'woocommerce'), array('a' => array('href' => array()))),
+				esc_url(wc_logout_url())
+			);
+			?>
+		</p>
+	</div>
 
-<p>
-	<?php
-	/* translators: 1: Orders URL 2: Address URL 3: Account URL. */
-	$dashboard_desc = __( 'From your account dashboard you can view your <a href="%1$s">recent orders</a>, manage your <a href="%2$s">billing address</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce' );
-	if ( wc_shipping_enabled() ) {
-		/* translators: 1: Orders URL 2: Addresses URL 3: Account URL. */
-		$dashboard_desc = __( 'From your account dashboard you can view your <a href="%1$s">recent orders</a>, manage your <a href="%2$s">shipping and billing addresses</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce' );
-	}
-	printf(
-		wp_kses( $dashboard_desc, $allowed_html ),
-		esc_url( wc_get_endpoint_url( 'orders' ) ),
-		esc_url( wc_get_endpoint_url( 'edit-address' ) ),
-		esc_url( wc_get_endpoint_url( 'edit-account' ) )
-	);
-	?>
-</p>
+	<div class="custom-account-options">
+		<p>
+			<?php
+			$dashboard_desc = __('From here, you can view your <a href="%1$s">orders</a>, manage <a href="%2$s">addresses</a>, and <a href="%3$s">update account details</a>.', 'woocommerce');
+			printf(
+				wp_kses($dashboard_desc, array('a' => array('href' => array()))),
+				esc_url(wc_get_endpoint_url('orders')),
+				esc_url(wc_get_endpoint_url('edit-address')),
+				esc_url(wc_get_endpoint_url('edit-account'))
+			);
+			?>
+		</p>
+	</div>
+</div>
 
 <?php
-	/**
-	 * My Account dashboard.
-	 *
-	 * @since 2.6.0
-	 */
-	do_action( 'woocommerce_account_dashboard' );
+do_action('woocommerce_account_dashboard');
+?>
 
-	/**
-	 * Deprecated woocommerce_before_my_account action.
-	 *
-	 * @deprecated 2.6.0
-	 */
-	do_action( 'woocommerce_before_my_account' );
-
-	/**
-	 * Deprecated woocommerce_after_my_account action.
-	 *
-	 * @deprecated 2.6.0
-	 */
-	do_action( 'woocommerce_after_my_account' );
-
-/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
+<?php
+echo '<h2 class="best-sellers-heading">Best Sellers</h2>';
+echo do_shortcode('[products limit="4" best_selling="true"]');
+?>
